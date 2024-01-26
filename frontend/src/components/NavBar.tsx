32de0665b9d232c4cc84be440758070b4adc5289
Thean <theantian089@gmail.com>
2024-01-26 22:11:20 +0700

    Update title and description in metadata

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -9,8 +9,8 @@ const NavBar = async () => {
   const session = await getAuthSession();
 
   return (
-    <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2">
-      <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl">
+    <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] border-b border-zinc-300">
+      <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl py-3">
         <Link href={"/"} className="flex items-center gap-2">
           <p className="rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white">
             QuizzMeAI
